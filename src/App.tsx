502dc6763503f14d0de9984f99796f6c0778f6be
Lucas4MouthGauard <biaoli888@outlook.com>
2025-08-12 11:44:45 +0800

    feat: Update X/Twitter and CA command outputs
    - Change X/Twitter command output to '@https://x.com/StupidAI_meme'
    - Change CA command output to 'Coming Soon'
    - Both commands maintain 'Artificial Stupidity > Artificial Intelligence' motto
    - Commands support multiple case variations (x/X, twitter/Twitter, ca/Ca/CA)

--- src/App.tsx
+++ src/App.tsx
@@ -413,11 +413,11 @@ const App: React.FC = () => {
               case 'ca':
       case 'Ca':
       case 'CA':
-          output = 'CertzzhwnUofK38g9vmUcL9tkeXPxotxrJpCZZA4pump\n\nArtificial Stupidity > Artificial Intelligence';
+          output = 'Coming Soon\n\nArtificial Stupidity > Artificial Intelligence';
         break;
       case 'x':
       case 'twitter':
-        output = '@https://x.com/StupidAI_system\n\nArtificial Stupidity > Artificial Intelligence';
+        output = '@https://x.com/StupidAI_meme\n\nArtificial Stupidity > Artificial Intelligence';
         break;
       case '$vinedoge':
       case 'vinedoge':
